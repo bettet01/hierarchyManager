cc50d9e9a90bcfc2a2ad50aa6b3e7c084fe3b5a3
Ethan Bettenga <bettet01@luther.edu>
2019-11-22 15:53:31 -0600

    add save file
    need node.js to create a function that allows the permanent overwrite of the save file.

--- src/index.js
+++ src/index.js
@@ -8,91 +8,11 @@ import ManagerBox from "./components/ManagerBox/ManagerBox";
 import ManagerInputBox from "./components/ManagerInputBox/ManagerInputBox";
 import RemoveButton from "../src/components/RemoveButton/RemoveButton";
 import AddButton from "./components/AddButton/AddButton";
-
-const object = {
-  "Famatest Zest": {
-    name: "Famatest Zest",
-    cycles: {
-      "AP's": {
-        cycleName: "AP's",
-        employees: {
-          "John Billtest": {
-            firstName: "John",
-            lastName: "Billtest",
-            middleInitial: "R",
-            username: "testuserid"
-          },
-          "Lindsey Blonde": {
-            firstName: "Lindsey",
-            lastName: "Blonde",
-            middleInitial: "R",
-            username: "testuserid"
-          }
-        }
-      },
-      "Cycle 5": {
-        cycleName: "Cycle 5",
-        employees: {
-          "Devon Gotem": {
-            firstName: "Devon",
-            lastName: "Gotem",
-            middleInitial: "R",
-            username: "testuserid"
-          },
-          "Max Kemp": {
-            firstName: "Max",
-            lastName: "Kemp",
-            middleInitial: "R",
-            username: "testuserid"
-          }
-        }
-      }
-    }
-  },
-  "Gene Diest": {
-    name: "Gene Diest",
-    cycles: {
-      LVAR: {
-        cycleName: "LVAR",
-        employees: {
-          "Ethan Bettenga": {
-            firstName: "Ethan",
-            lastName: "Bettenga",
-            middleInitial: "R",
-            username: "testuserid"
-          },
-          "Jackie Cychosz": {
-            firstName: "Jackie",
-            lastName: "Cychosz",
-            middleInitial: "R",
-            username: "testuserid"
-          }
-        }
-      },
-      "Cycle 6": {
-        cycleName: "Cycle 6",
-        employees: {
-          "Erik Ehresmann": {
-            firstName: "Erik",
-            lastName: "Ehresmann",
-            middleInitial: "R",
-            username: "testuserid"
-          },
-          "Jennaya Willgo": {
-            firstName: "Jennaya",
-            lastName: "Willgo",
-            middleInitial: "R",
-            username: "testuserid"
-          }
-        }
-      }
-    }
-  }
-};
+import object from "../savefile.csv";
 
 function App() {
   // Setting State
-  const [columnValues, setColumnValues] = useState('');
+  const [columnValues, setColumnValues] = useState(object);
   const [managerValue, setManagerValue] = React.useState("");
 
   /* Functions i'm planning to prop drill down to needed values */
